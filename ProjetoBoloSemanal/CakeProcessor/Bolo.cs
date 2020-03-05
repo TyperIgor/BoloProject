0acd163ea2d3b405d269ae74c17ac592715c1109
TyperIgor <igor_purosso@hotmail.com>
2020-03-05 17:21:40 -0300

    creates more layers

---
+++ ProjetoBoloSemanal/CakeProcessor/Bolo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoBoloSemanal.CakeDates;
+using ProjetoBoloSemanal.CakeOrder;
+
+
+namespace ProjetoBoloSemanal
+{
+    public abstract class BoloImplementations
+    {
+        StreamReader _ReadPath = new StreamReader(@"C:\Users\igor.lima\Documents\bolosemanal.txt");
+
+        public StreamReader ReadFile { get { return _ReadPath; } }
+
+        List<string> _namesList = new List<string>();
+
+        public List<string> ListNames { get { return _namesList; } }
+    }
+
+    public class CakeProcessor : BoloImplementations
+    {
+        public void ReadStreamReaderFileToList()
+        {
+            while (!ReadFile.EndOfStream)
+            {
+                string Ler;
+
+                Ler = ReadFile.ReadLine();
+
+                ListNames.Add(Ler);
+            }
+        }
+    }
+
+    public class PrintFile : CakeProcessor
+    {
+        public void PrintBolo()
+        {
+            ReadStreamReaderFileToList();
+
+            OrderString.BubbleSort(ListNames);
+                                     
+            var n = 0;
+            foreach (var date in Dates.GetDates())
+            {
+                if (n == ListNames.Count)
+                    n = 0;
+
+                Console.WriteLine("Nomes: {0}  Bolo: {1} ", ListNames[n], date);
+                n++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+   
+    
+}
