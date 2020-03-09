2778c01139b4ada3bb22487301f09cf9b2b75dde
TyperIgor <igor_purosso@hotmail.com>
2020-03-09 13:43:08 -0300

    creates layer that contains the cake procediments

---
+++ CakeProcessors/CakeFileRead/CakeFileProcessorRead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectBolo.CakeDomain;
+
+namespace ProjectBolo.CakeProcessors
+{
+    public class CakeFile : CakeEntities
+    {
+        public void ReadNamesFilesToList()
+        {
+            while (!ReadFile.EndOfStream)
+            {
+                string ReadNames;
+
+                ReadNames = ReadFile.ReadLine();
+
+                namesList.Add(ReadNames);
+            }
+            
+        }
+    }
+}
