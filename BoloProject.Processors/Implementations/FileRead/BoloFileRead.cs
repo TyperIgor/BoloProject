89cc01efff63eb53b562323f92c31cdefca99bd6
TyperIgor <igor_purosso@hotmail.com>
2020-03-15 20:52:15 -0300

    Creates layer with the bolo implemetations and your responsabilities

---
+++ BoloProject.Processors/Implementations/FileRead/BoloFileRead.cs
@@ -0,0 +1,19 @@
+﻿using BoloProject.Domain.Entities;
+
+namespace BoloProject.Processors.Implementations.FileRead
+{
+    public class BoloFileRead : BoloEntity
+    {
+        public void ReadFileNamesToList()
+        {
+            while(!ReadFile.EndOfStream)
+            {
+                string ReadNames;
+
+                ReadNames = ReadFile.ReadLine();
+
+                Names.Add(ReadNames);
+            }
+        }
+    }
+}
