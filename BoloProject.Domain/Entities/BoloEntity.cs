499740525e6d6fcd365d14ccf60cf3ef8d3e5353
TyperIgor <igor_purosso@hotmail.com>
2020-03-16 12:26:07 -0300

    creates bolo layer domain and your entity classe

---
+++ BoloProject.Domain/Entities/BoloEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace BoloProject.Domain.Entities
+{
+    public abstract class BoloEntity
+    {
+        private StreamReader _fileNames = new StreamReader(@"C:\Users\igor.lima\Documents\bolosemanal.txt");
+
+        private List<string> _names = new List<string>();
+
+        public StreamReader ReadFile { get { return _fileNames; } }
+
+        public List<string> Names { get { return _names; } }
+    }
+}
