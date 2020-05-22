3b1bbe6fcf8814dde17f33db55d0d31d2e6bb1dc
TyperIgor <igor_purosso@hotmail.com>
2020-05-22 14:29:30 -0300

    Adds core layers from the bolo integration

---
+++ Bolo.Integration.Core/Processors/FileRead/Path.cs
@@ -0,0 +1,19 @@
+﻿using Bolo.Integration.Domain.Entities;
+
+namespace Bolo.Integration.Core.Processors.FileRead
+{
+    public class Path : FileAtributes
+    {
+        public void ReadFileNamesToList()
+        {
+            while (!ReadFile.EndOfStream)
+            {
+                string readNames;
+
+                readNames = ReadFile.ReadLine();
+
+                NamesList.Add(readNames);
+            }
+        }
+    }
+}
