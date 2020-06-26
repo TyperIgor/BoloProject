91d83fd008654966f7a5ce50c867d4267c04f037
IGorDEVOP <igor.lima@stone.com.br>
2020-06-25 22:58:30 -0300

    inserts new bolo updates

---
+++ Projeto.BoloSemanal/Project.BoloSemanal.Core/Excel/OperateExcel.cs
@@ -0,0 +1,44 @@
+﻿using Bytescout.Spreadsheet;
+using Project.BoloSemanal.Core.Dates;
+using Project.BoloSemanal.Core.Processors.FileRead;
+
+namespace Project.BoloSemanal.Core.Excel
+{
+    public class OperateExcel
+    {
+        public void ExecuteExcel(Path path)
+        {
+            Spreadsheet spreadsheet = new Spreadsheet();
+            Worksheet sheets = spreadsheet.Workbook.Worksheets.Add("Bolinho");
+
+            var Datas = FindDates.GetDatas();
+            var ListNames = path.NamesList;
+
+            sheets.Cell(0, 0).Value = "Nomes:";
+            sheets.Cell(0, 1).Value = "Datas:";
+
+            sheets.Columns[0].Width = 200;
+            sheets.Columns[1].Width = 150;
+
+            var line = 0;
+            var indexList = 0;
+
+            foreach (var dates in Datas)
+            {
+                if (indexList == ListNames.Count)
+                    indexList = 0;
+
+                sheets.Cell(line + 1, 1).Value = dates;
+                sheets.Cell(line + 1, 0).Value = ListNames[indexList];
+
+                line++;
+                indexList++;
+            }
+
+            spreadsheet.SaveAs("ProximoBolo.xlsx");
+            spreadsheet.Close();
+        }
+
+
+    }
+}
---
+++ Projeto.BoloSemanal/Project.BoloSemanal.GoogleSheets/Program.cs
@@ -0,0 +1,13 @@
+﻿using Project.BoloSemanal.GoogleSheets.Initializer;
+
+namespace Project.BoloSemanal.GoogleSheets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteSheetProcessor x = new ExecuteSheetProcessor();
+            x.Start();
+        }
+    }
+}
